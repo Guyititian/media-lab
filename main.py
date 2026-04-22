7eabc9346a93de0f4a9ad30262e2a2b7b6cc8edc
Guyititian <128245262+Guyititian@users.noreply.github.com>
2026-04-21 20:08:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import os
+
 from api.routes import router
 
 app = FastAPI()
 
+# -------------------------
+# API ROUTES
+# -------------------------
 app.include_router(router)
+
+# -------------------------
+# OUTPUT FILE STORAGE
+# -------------------------
+os.makedirs("outputs", exist_ok=True)
+
+app.mount(
+    "/outputs",
+    StaticFiles(directory="outputs"),
+    name="outputs"
+)
